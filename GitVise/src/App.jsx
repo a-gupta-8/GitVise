4f9a83c318c7acb411d9136928e1923bcc4dba91
arsh-gupta-8 <arshmailing@gmail.com>
2026-07-18 22:16:47 +0700

    Added text input for git queries

--- GitVise/src/App.jsx
+++ GitVise/src/App.jsx
@@ -9,6 +9,20 @@ const initialNodes = [
 const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
  
 export default function App() {
+
+  const [text, setText] = useState("");
+
+  const checkEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      submitGitQuery(text);
+      event.preventDefault();
+    };
+  };
+
+  const submitGitQuery = (query) => {
+    console.log(query);
+  };
+
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
  
@@ -26,7 +40,32 @@ export default function App() {
   );
  
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ 
+      width: '100%', 
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center', 
+    }}>
+      <div style={{ padding: '12px' }}>
+        <input 
+          type='text' 
+          id='git-query' 
+          placeholder='Enter git command'
+          value={text} 
+          onChange={((e) => setText(e.target.value))}
+          onKeyDown={(checkEnterKey)}
+          required
+          style={{
+            padding: '12px',
+            fontSize: '15px',
+            border: '1px solid #6b6b6b',
+            borderRadius: '8px',
+            outline: 'none',
+        }}>
+        </input>
+      </div>
+      
       <ReactFlow
         nodes={nodes}
         edges={edges}
